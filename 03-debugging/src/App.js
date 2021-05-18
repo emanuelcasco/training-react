8f938ed45defe878e59ea50d01a8f6662414d77a
Emanuel Casco <cascoemanuel@gmail.com>
2021-05-17 21:45:31 -0300

    feat: debugging

---
+++ 03-debugging/src/App.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import { v4 as uuid } from 'uuid';
+
+import CourseGoalList from './components/CourseGoals/CourseGoalList/CourseGoalList';
+import CourseInput from './components/CourseGoals/CourseInput/CourseInput';
+import './App.css';
+
+const App = () => {
+  const [courseGoals, setCourseGoals] = useState([
+    { text: 'Do all exercises!', id: uuid() },
+    { text: 'Finish the course!', id: uuid() },
+  ]);
+
+  const addGoalHandler = (enteredText) => {
+    setCourseGoals((prevGoals) => {
+      const updatedGoals = [...prevGoals];
+      updatedGoals.unshift({ text: enteredText, id: uuid() });
+      return updatedGoals;
+    });
+  };
+
+  const deleteItemHandler = (goalId) => {
+    setCourseGoals((prevGoals) => {
+      const updatedGoals = prevGoals.filter((goal) => goal.id !== goalId);
+      return updatedGoals;
+    });
+  };
+
+  let content = (
+    <p style={{ textAlign: 'center' }}>No goals found. Maybe add one?</p>
+  );
+
+  if (courseGoals.length > 0) {
+    content = (
+      <CourseGoalList items={courseGoals} onDeleteItem={deleteItemHandler} />
+    );
+  }
+
+  return (
+    <section>
+      <div id="goal-form">
+        <CourseInput onAddGoal={addGoalHandler} />
+      </div>
+      <div id="goals">{content}</div>
+    </section>
+  );
+};
+
+export default App;
